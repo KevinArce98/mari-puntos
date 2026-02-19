302d410d8787497275f54018234305133366336e
Kevin Arias <arias9068@gmail.com>
2026-02-19 15:52:48 -0600

    chore: update ESLint configuration and upgrade TypeScript ESLint dependencies
    - Added node_modules, android, and ios directories to ESLint ignore patterns
    - Disabled problematic import-related ESLint rules to prevent false positives
    - Upgraded @typescript-eslint packages from 8.53.1 to 8.56.0
    - Updated ESLint peer dependency support to include version 10.x
    - Reorganized package-lock.json dependencies under eslint-config-expo namespace

--- mari-puntos-app/eslint.config.js
+++ mari-puntos-app/eslint.config.js
@@ -7,6 +7,17 @@ module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', 'node_modules/*', 'android/*', 'ios/*'],
+  },
+  {
+    rules: {
+      'import/export': 'off',
+      'import/namespace': 'off',
+      'import/default': 'off',
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-unresolved': 'off',
+      'import/no-duplicates': 'off',
+    },
   },
 ]);
